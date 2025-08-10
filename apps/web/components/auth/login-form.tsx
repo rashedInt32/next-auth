2a15891576ee92c6ec38670c58393ac4ff3f239c
rashed parvez <parvez08eee@gmail.com>
2025-08-10 07:33:08 +0600

    Add email password fields for login

--- apps/web/components/auth/login-form.tsx
+++ apps/web/components/auth/login-form.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useForm } from "react-hook-form";
 import { CardWrapper } from "./card-wrapper";
-import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
@@ -17,12 +16,47 @@ import { FormError } from "./form-error";
 import { FormSuccess } from "./form-success";
 
 export const LoginForm = () => {
+  const form = useForm();
+  const onSubmit = () => {};
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
       backButtonHref="/auth/register"
       backButtonLabel="Don't have an account? Register"
       showSocial
-    ></CardWrapper>
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <>
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="example@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="******" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              </>
+            )}
+          />
+          <FormError message="" />
+          <FormSuccess message="" />
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
   );
 };
