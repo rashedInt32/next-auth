6c00172c17e632630c1bee9f0d352953ba564f73
rashed parvez <parvez08eee@gmail.com>
2025-08-25 16:31:54 +0600

    Reset password component

--- apps/web/components/auth/reset-password-form.tsx
+++ apps/web/components/auth/reset-password-form.tsx
@@ -18,12 +18,9 @@ import {
   resetPasswordSchema,
   ResetPasswordResolver,
 } from "@/schema/authSchema";
-import { useState } from "react";
-import { Mail, Loader2 } from "lucide-react";
+import { Loader2 } from "lucide-react";
 
 export const ResetPasswordForm = () => {
-  const [success, setSuccess] = useState(false);
-
   const form = useForm<resetPasswordSchema>({
     resolver: effectTsResolver(ResetPasswordResolver),
     defaultValues: {
@@ -38,90 +35,76 @@ export const ResetPasswordForm = () => {
   };
 
   const resetForm = () => {
-    setSuccess(false);
     form.reset();
   };
 
   return (
     <CardWrapper
-      headerLabel="Enter your email to get reset link"
+      headerLabel="Enter your new password"
       backButtonHref="/auth/login"
       backButtonLabel="Go back to Login"
       showSocial={false}
     >
-      {!success ? (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="token"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input type="text" {...field} hidden />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="token"
+            render={({ field }) => (
+              <FormItem className="h-0 m-0">
+                <FormControl>
+                  <Input type="text" {...field} hidden />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} placeholder="********" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <FormField
-              control={form.control}
-              name="configmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Configm Password</FormLabel>
-                  <FormControl>
-                    <Input type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={form.control}
+            name="configmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} placeholder="********" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-            <FormError message={form.formState?.errors?.root?.message} />
+          <FormError message={form.formState?.errors?.root?.message} />
 
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={form.formState.isSubmitting}
-            >
-              {form.formState.isSubmitting ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Sending...
-                </>
-              ) : (
-                "Submit"
-              )}
-            </Button>
-          </form>
-        </Form>
-      ) : (
-        <div className="flex flex-col items-center space-y-4 text-center">
-          <Mail className="w-12 h-12 text-green-500" />
-          <FormSuccess message="We sent you a reset link. Please check your email." />
-          <button
-            onClick={resetForm}
-            className="text-sm text-blue-500 hover:underline"
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={form.formState.isSubmitting}
           >
-            Resend email
-          </button>
-        </div>
-      )}
+            {form.formState.isSubmitting ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Sending...
+              </>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
