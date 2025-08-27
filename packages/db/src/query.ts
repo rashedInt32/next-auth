93e358cd0ad5db137aee1345c02326cdb003a587
rashed parvez <parvez08eee@gmail.com>
2025-08-27 15:06:36 +0600

    Added query function doc commnets

--- packages/db/src/query.ts
+++ packages/db/src/query.ts
@@ -1,24 +1,42 @@
 import { Effect } from "effect";
 import { PrismaService, prismaOp } from "./prisma-service";
 
+/**
+ * Find user by email
+ * @param email
+ * @returns user or null
+ */
 export const findUserByEmail = (email: string) =>
   Effect.gen(function* () {
     const prisma = yield* PrismaService;
     return yield* prismaOp(prisma.user.findUnique)({ where: { email } });
   });
 
+/**
+ * Find user by id
+ * @param id
+ * @returns user or null
+ */
 export const findUserById = (id: string) =>
   Effect.gen(function* () {
     const prisma = yield* PrismaService;
     return yield* prismaOp(prisma.user.findUnique)({ where: { id } });
   });
 
+/** Create a new user
+ * @param data - user data
+ * @returns created user
+ */
 export const createUser = (data: Record<string, any>) =>
   Effect.gen(function* () {
     const prisma = yield* PrismaService;
     return yield* prismaOp(prisma.user.create)({ data });
   });
 
+/** Create a password reset token
+ * @param data - token data
+ * @returns created token
+ */
 export const createPasswordResetToken = (data: {
   email: string;
   token: string;
@@ -29,6 +47,10 @@ export const createPasswordResetToken = (data: {
     return yield* prismaOp(prisma.passwordResetToken.create)({ data });
   });
 
+/** Delete password reset tokens by email
+ * @param data - email
+ * @returns number of deleted tokens
+ */
 export const deletePasswordResetToken = (data: { email: string }) =>
   Effect.gen(function* () {
     const prisma = yield* PrismaService;
@@ -37,6 +59,10 @@ export const deletePasswordResetToken = (data: { email: string }) =>
     });
   });
 
+/** Find a password reset token by token string
+ * @param token - token string
+ * @returns token or null
+ */
 export const findResetToken = (token: string) =>
   Effect.gen(function* () {
     const prisma = yield* PrismaService;
