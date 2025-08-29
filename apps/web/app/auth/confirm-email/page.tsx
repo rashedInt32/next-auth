4113c72aed5f15860acaa1cf8bc380ae14ed45bf
rashed parvez <parvez08eee@gmail.com>
2025-08-29 15:37:19 +0600

    update email routes

--- apps/web/app/auth/confirm-email/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ConfirmEmail } from "@/components/auth/confirm-email";
-import { Suspense } from "react";
-export default function ConfirmEmailPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ConfirmEmail />
-    </Suspense>
-  );
-}
